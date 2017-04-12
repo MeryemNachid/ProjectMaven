ba155d1966c53a9b0ff29d90d6690fd28b1e0780
Nachid Meryem <mnachid@SA203-07A.ensi-bourges.fr>
2017-04-12 09:40:08 +0200

    Génération de fichiers

---
+++ mn/src/main/java/MN_maven/App.java
@@ -0,0 +1,13 @@
+package MN_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
