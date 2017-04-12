90c8c723540a3e623a083b8effd592f9b1336541
Nachid Meryem <mnachid@SA203-07A.ensi-bourges.fr>
2017-04-12 16:56:24 +0200

    Parsing du fichier .csv

--- mn/src/main/java/MN_maven/App.java
+++ mn/src/main/java/MN_maven/App.java
@@ -1,5 +1,12 @@
 package MN_maven;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
+
 /**
  * Hello world!
  *
@@ -9,11 +16,27 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Test Eclipse - exercice 7");
+        //System.out.println("Test Eclipse - exercice 7");
         
-        int maximum = max(2,5);
+        //int maximum = max(2,5); 
+        //System.out.println(maximum);
         
-        System.out.println(maximum);
+        App app = new App();
+        
+        int monmax = 0;
+        try {
+        	CSVReader reader = new CSVReader(new FileReader("data.csv"));
+        	
+        	List<String[]> myEntries = reader.readAll();
+        
+        	for(String[] l : myEntries){
+        		for(String nb : l){
+        			System.out.println("Nombre lu " + nb);
+        		}
+        	}
+        } catch (IOException e){
+        	e.printStackTrace();
+        }
     }
     
     public static int max(int a, int b){
