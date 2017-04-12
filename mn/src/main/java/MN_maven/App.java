bf7f528edb3f461f2f622f86c8de1fe34f2ade49
Nachid Meryem <mnachid@SA203-07A.ensi-bourges.fr>
2017-04-12 13:48:34 +0200

    Tests unitaires et construction de la release

--- mn/src/main/java/MN_maven/App.java
+++ mn/src/main/java/MN_maven/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Test Eclipse - exercice 7");
+        
+        int maximum = max(4,5);
+        
+        System.out.println(maximum);
+    }
+    
+    public static int max(int a, int b){
+    	return a>b ? a:b ;
     }
 }
