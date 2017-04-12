b6bfd9f27f4bf3f488f8ab1f8e7e5270315abdf2
Nachid Meryem <mnachid@SA203-07A.ensi-bourges.fr>
2017-04-12 14:06:22 +0200

    Création du répertoire releases

--- mn/src/main/java/MN_maven/App.java
+++ mn/src/main/java/MN_maven/App.java
@@ -11,12 +11,13 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Test Eclipse - exercice 7");
         
-        int maximum = max(4,5);
+        int maximum = max(2,5);
         
         System.out.println(maximum);
     }
     
     public static int max(int a, int b){
     	return a>b ? a:b ;
+    	//return a;
     }
 }
